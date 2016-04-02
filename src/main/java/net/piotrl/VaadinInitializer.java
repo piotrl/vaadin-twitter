9e26b00745e060be2093aad8bf8fbe18e7e7af4c
Piotr Lewandowski <grovman@gmail.com>
2016-04-02 14:16:38 +0200

    Add edit form

--- src/main/java/net/piotrl/VaadinInitializer.java
+++ src/main/java/net/piotrl/VaadinInitializer.java
@@ -2,10 +2,12 @@ package net.piotrl;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 import net.piotrl.imports.Customer;
+import net.piotrl.imports.CustomerEditor;
 import net.piotrl.imports.CustomerRepository;
 import net.piotrl.imports.UploadInfoWindow;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,24 +19,67 @@ import java.util.List;
 @Theme("valo")
 public class VaadinInitializer extends UI {
 
-    UploadInfoWindow uploadInfoWindow;
-    Grid grid;
-    CustomerRepository repo;
+    private final CustomerRepository repo;
+    private final CustomerEditor editor;
+
+    private final Grid grid;
+    private final TextField filter;
+    private final Button addNewBtn;
+    private UploadInfoWindow uploadInfoWindow;
 
     @Autowired
-    public VaadinInitializer(CustomerRepository repo) {
+    public VaadinInitializer(CustomerRepository repo, CustomerEditor editor) {
         this.repo = repo;
+        this.editor = editor;
         this.grid = new Grid();
+        this.filter = new TextField();
+        this.addNewBtn = new Button("New customer", FontAwesome.PLUS);
     }
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        setContent(grid);
-        TextField filter = new TextField();
-        filter.setInputPrompt("Filter by last name");
-        filter.addTextChangeListener(e -> listCustomers(e.getText()));
-        VerticalLayout mainLayout = new VerticalLayout(filter, grid);
+        // build layout
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
+        VerticalLayout mainLayout = new VerticalLayout(actions, grid, editor);
         setContent(mainLayout);
+
+        // Configure layouts and components
+        actions.setSpacing(true);
+        mainLayout.setMargin(true);
+        mainLayout.setSpacing(true);
+
+        grid.setHeight(300, Unit.PIXELS);
+        grid.setColumns("id", "firstName", "lastName");
+
+        filter.setInputPrompt("Filter by last name");
+
+        // Hook logic to components
+
+        // Replace listing with filtered content when user changes filter
+        filter.addTextChangeListener(e -> listCustomers(e.getText()));
+
+        // Connect selected Customer to editor or hide if none is selected
+        grid.addSelectionListener(e -> {
+            if (e.getSelected().isEmpty()) {
+                editor.setVisible(false);
+            }
+            else {
+                editor.editCustomer((Customer) grid.getSelectedRow());
+            }
+        });
+
+
+        // Instantiate and edit new Customer the new button is clicked
+        addNewBtn.addClickListener(e -> editor.editCustomer(new Customer("", "")));
+
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            listCustomers(filter.getValue());
+        });
+
+        // Initialize listing
+        listCustomers(null);
     }
 
     private void listCustomers(String text) {
