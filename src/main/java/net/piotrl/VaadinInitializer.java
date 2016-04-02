0211b12b7269cd8c5fbc72fba8ea0452e3c043c5
Piotr Lewandowski <grovman@gmail.com>
2016-04-02 14:03:30 +0200

    Add filter option

--- src/main/java/net/piotrl/VaadinInitializer.java
+++ src/main/java/net/piotrl/VaadinInitializer.java
@@ -4,13 +4,14 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-import com.vaadin.ui.Grid;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.Upload;
+import com.vaadin.ui.*;
 import net.piotrl.imports.Customer;
 import net.piotrl.imports.CustomerRepository;
 import net.piotrl.imports.UploadInfoWindow;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @SpringUI
 @Theme("valo")
@@ -29,12 +30,23 @@ public class VaadinInitializer extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         setContent(grid);
-        listCustomers();
+        TextField filter = new TextField();
+        filter.setInputPrompt("Filter by last name");
+        filter.addTextChangeListener(e -> listCustomers(e.getText()));
+        VerticalLayout mainLayout = new VerticalLayout(filter, grid);
+        setContent(mainLayout);
     }
 
-    private void listCustomers() {
+    private void listCustomers(String text) {
+        List<Customer> customers;
+        if (StringUtils.isEmpty(text)) {
+            customers = repo.findAll();
+        } else {
+            customers = repo.findByLastNameStartsWithIgnoreCase(text);
+        }
+
         grid.setContainerDataSource(
-                new BeanItemContainer(Customer.class, repo.findAll()));
+                new BeanItemContainer(Customer.class, customers));
     }
 
     private Upload getUploadButton() {
