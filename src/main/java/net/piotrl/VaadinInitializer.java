c9c4c8d0be783eb0a0b4d408a0f6735e6da9a5cf
Piotr Lewandowski <grovman@gmail.com>
2016-04-02 13:26:29 +0200

    Configure database repository and @Entity

--- src/main/java/net/piotrl/VaadinInitializer.java
+++ src/main/java/net/piotrl/VaadinInitializer.java
@@ -1,22 +1,40 @@
 package net.piotrl;
 
 import com.vaadin.annotations.Theme;
+import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.Upload;
+import net.piotrl.imports.Customer;
+import net.piotrl.imports.CustomerRepository;
 import net.piotrl.imports.UploadInfoWindow;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringUI
 @Theme("valo")
 public class VaadinInitializer extends UI {
 
     UploadInfoWindow uploadInfoWindow;
+    Grid grid;
+    CustomerRepository repo;
+
+    @Autowired
+    public VaadinInitializer(CustomerRepository repo) {
+        this.repo = repo;
+        this.grid = new Grid();
+    }
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        Upload upload = getUploadButton();
-        setContent(upload);
+        setContent(grid);
+        listCustomers();
+    }
+
+    private void listCustomers() {
+        grid.setContainerDataSource(
+                new BeanItemContainer(Customer.class, repo.findAll()));
     }
 
     private Upload getUploadButton() {
